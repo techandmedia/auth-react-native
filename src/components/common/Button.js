ba9951690b7a1fa39c844642f6c4558eec645582
techandmedia <eko.andri@icloud.com>
2018-08-27 10:51:02 +0700

    add styled-components

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
+import styled from 'styled-components';
 
 const Button = () => {
   const { buttonStyle, textStyle } = styles;
 
   return (
-    <TouchableOpacity style={buttonStyle}>
-      <Text style={textStyle}>
+    <StyledButton>
+      <StyledText>
         Click me!!!
-      </Text>
-    </TouchableOpacity>
+      </StyledText>
+    </StyledButton>
   );
 };
+
+const StyledButton = styled.TouchableOpacity`
+  flex: 1;
+  align-self: stretch;
+  background-color: #fff;
+  border-radius: 5;
+  border-width: 1;
+  border-color: #007aff;
+  margin-left: 5;
+  margin-right: 5;
+`
+
+const StyledText = styled.Text`
+  align-self: center;
+  color: #007aff;
+  font-size: 16;
+  font-weight: 600;
+  padding-top: 10;
+  padding-bottom: 10;
+`
 
 const styles = {
   textStyle: {
